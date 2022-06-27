9cf7f3803c841fb2eb9f92458a22f967d57fc925
Berk Disli <berkdisli@yahoo.com>
2022-06-27 14:59:33 +0200

    imgs, intro update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,38 @@
 import "./Intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Engineer"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/berk.jpeg" alt="berk" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Berk Disli</h1>
+          <h3>
+            Full Stack <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/arrowDown.png" alt="arrow-down" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -2,18 +2,18 @@ import "./Menu.scss";
 
 export default function Menu(menuOpen, setMenuOpen) {
   return (
-    <div id="menu" className={"menu" + (menuOpen && "active")}>
+    <div className={"menu" + (menuOpen && "active")}>
       <ul>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#intro">Home</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#works">Works</a>
         </li>
-        <li>
+        <li onClick={() => setMenuOpen(false)}>
           <a href="#contact">Contact</a>
         </li>
       </ul>
