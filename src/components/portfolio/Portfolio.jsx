259f65c5ba0f6d7685f38a5e2ce36b996a0eea92
Berk Disli <berkdisli@yahoo.com>
2022-06-29 17:59:55 +0200

    some changes on portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,15 +3,54 @@ import "./Portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      <h1>Porfolio</h1>
+      <h1>Portfolio</h1>
       <ul>
-        <li>Featured</li>
+        <li className="active">Featured</li>
+        <li>MyFlix</li>
         <li>Featured</li>
         <li>Featured</li>
       </ul>
       <div className="container">
         <div className="item">
-          <img src="" alt="" />
+          <img
+            src="https://images.saymedia-content.com/.image/t_share/MTc0NDU1NjM4NjA0MzkyMDcw/cutest-pokemon-of-all-time.png"
+            alt="myflixpng"
+          />
+          <h3>Banking</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
+            alt="myflixpng"
+          />
+          <h3>Banking</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
+            alt="myflixpng"
+          />
+          <h3>Banking</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
+            alt="myflixpng"
+          />
+          <h3>Banking</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
+            alt="myflixpng"
+          />
+          <h3>Banking</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
+            alt="myflixpng"
+          />
           <h3>Banking</h3>
         </div>
       </div>
