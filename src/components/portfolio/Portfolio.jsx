dcb779bfc1d289c4f65f7bc191b0490116d2c17e
Berk Disli <berkdisli@yahoo.com>
2022-07-01 15:40:05 +0200

    data.js, portfoliolist are created, some changes on portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,58 +1,55 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./Portfolio.scss";
+import { webPortfolio, mobilePortfolio } from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("web");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "web":
+        setData(mobilePortfolio);
+        break;
+      default:
+        setData(webPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>MyFlix</li>
-        <li>Featured</li>
-        <li>Featured</li>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img
-            src="https://images.saymedia-content.com/.image/t_share/MTc0NDU1NjM4NjA0MzkyMDcw/cutest-pokemon-of-all-time.png"
-            alt="myflixpng"
-          />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
-            alt="myflixpng"
-          />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
-            alt="myflixpng"
-          />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
-            alt="myflixpng"
-          />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
-            alt="myflixpng"
-          />
-          <h3>Banking</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://static.wikia.nocookie.net/pokemon/images/8/88/Char-Eevee.png/revision/latest/smart/width/250/height/250?cb=20190625223735"
-            alt="myflixpng"
-          />
-          <h3>Banking</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="myflixpng" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const webPortfolio = [
+    {
+        id: 1,
+        title: "MyFlix App",
+        img: "assets/berk.jpeg"
+    },
+    {
+        id: 2,
+        title: "MyFlix App",
+        img: "assets/Myflix"
+    },
+    {
+        id: 3,
+        title: "MyFlix App",
+        img: "assets/Myflix"
+    },
+    {
+        id: 4,
+        title: "MyFlix App",
+        img: "assets/Myflix"
+    },
+    {
+        id: 5,
+        title: "MyFlix App",
+        img: "assets/Myflix"
+    },
+    {
+        id: 6,
+        title: "MyFlix App",
+        img: "assets/Myflix"
+    },
+
+
+
+];
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "Chit-Chat",
+        img: "assets/Myflix"
+    },
+]
