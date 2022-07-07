71df3b0a9b1c0830826c921f90a2563e699e48d3
Berk Disli <berkdisli@yahoo.com>
2022-07-07 16:57:22 +0200

    works

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,28 @@
 import "./Works.scss";
 
 export default function Works() {
-  return <div className="works" id="works"></div>;
+  return (
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/65/65680.png"
+                    alt="mobilepic"
+                  />
+                </div>
+                <h2>Title</h2>
+                <p>asfsagagasgas ahsfbsahfbsabfsaömnbfsamnbfmasnbf</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
