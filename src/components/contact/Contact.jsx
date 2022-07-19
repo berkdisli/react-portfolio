c6d6fe0c10a070e663932e5d7519bb6868e82117
Berk Disli <berkdisli@yahoo.com>
2022-07-19 14:22:58 +0200

    works css, creating contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,22 @@
 import "./Contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img
+          src="https://cdn.pixabay.com/photo/2015/02/27/08/12/handshake-651818__480.png"
+          alt=""
+        />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
