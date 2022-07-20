4c7bf34ca9eb63ec116a76778c5db3b889ed856f
Berk Disli <berkdisli@yahoo.com>
2022-07-20 11:11:34 +0200

    contact main created, works id2

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,14 @@
 import "./Contact.scss";
+import { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -11,10 +19,13 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
           <button type="submit">Send</button>
+          {message && (
+            <span>Thanks, I'll try to reply as soon as possible</span>
+          )}
         </form>
       </div>
     </div>
