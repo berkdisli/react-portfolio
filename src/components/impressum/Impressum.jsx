9e29f77bb8feb04d6725815a2c670b4971f3f431
Berk Disli <berkdisli@yahoo.com>
2022-07-31 10:52:49 +0200

    impressum updated

--- src/components/impressum/Impressum.jsx
+++ src/components/impressum/Impressum.jsx
@@ -4,20 +4,28 @@ import "./Impressum.scss";
 export default function Impressum() {
   return (
     <div id="impressum" className="impressum">
-      <h1>Impressum</h1>
-      <p>Angaben gemäß § 5 TMG</p>
-      <p>Berk Disli Lichtenberger Str. 10179 Berlin</p>
-      <p>
-        {" "}
-        <strong>Vertreten durch: </strong>
-        Berk Disli
-      </p>
-      Impressum der{" "}
-      <a href="https://www.kanzlei-hasselbach.de/">
-        <text>
-          Kanzlei Hasselbach, Rechtsanwälte für Arbeitsrecht und Familienrecht
-        </text>
-      </a>
+      <div className="left">
+        <img
+          src="https://img.freepik.com/premium-vector/documents-folder-with-stamp-text-contract-with-approval-stamp_349999-535.jpg?w=740"
+          alt=""
+        />
+      </div>
+      <div className="right">
+        <h1>Impressum</h1>
+        <p>Angaben gemäß § 5 TMG</p>
+        <p>Berk Disli Lichtenberger Str. 10179 Berlin</p>
+        <p>
+          {" "}
+          <strong>Vertreten durch: </strong>
+          Berk Disli
+        </p>
+        Impressum der{" "}
+        <a href="https://www.kanzlei-hasselbach.de/">
+          <text>
+            Kanzlei Hasselbach, Rechtsanwälte für Arbeitsrecht und Familienrecht
+          </text>
+        </a>
+      </div>
     </div>
   );
 }
